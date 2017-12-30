d7bf35d65b1a1528cf0a1941d838a182a312279c
Unknown <chris@dicarlosystems.ca>
2017-12-29 23:34:51 -0500

    Fixed deprecated scope attribute

--- resources/coreui/src/views/Dashboard.vue
+++ resources/coreui/src/views/Dashboard.vue
@@ -502,13 +502,13 @@
             :fields="tableFields"
             head-variant="default"
             >
-            <template slot="avatar" scope="item">
+            <template slot="avatar" slot-scope="item">
               <div class="avatar">
                 <img :src="item.value.url" class="img-avatar" alt="">
                 <span class="avatar-status" v-bind:class="{ 'bg-success': item.value.status == 'success',  'bg-warning': item.value.status == 'warning', 'bg-danger': item.value.status == 'danger', 'bg-secondary': item.value.status == '' }"></span>
               </div>
             </template>
-            <template slot="user" scope="item">
+            <template slot="user" slot-scope="item">
               <div>{{item.value.name}}</div>
               <div class="small text-muted">
                 <span>
@@ -517,10 +517,10 @@
                 </span> | Registered: {{item.value.registered}}
               </div>
             </template>
-            <template slot="country" scope="item">
+            <template slot="country" slot-scope="item">
               <img :src="item.value.flag" :alt="item.value.name" style="height:24px;">
             </template>
-            <template slot="usage" scope="item">
+            <template slot="usage" slot-scope="item">
               <div class="clearfix">
                 <div class="float-left">
                   <strong>{{item.value.value}}%</strong>
@@ -531,10 +531,10 @@
               </div>
               <b-progress class="progress-xs" v-model="item.value.value" :variant="variant(item.value.value)"></b-progress>
             </template>
-            <template slot="payment" scope="item">
+            <template slot="payment" slot-scope="item">
               <i :class="item.value.icon" style="font-size:24px"></i>
             </template>
-            <template slot="activity" scope="item">
+            <template slot="activity" slot-scope="item">
               <div class="small text-muted">Last login</div>
               <strong>{{item.value}}</strong>
             </template>
